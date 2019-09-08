19ae0e6d8ce946a88c245b7edbc0c14fd95814d6
Amal Agrawal <amalagrawal@amals-mbp-2.bc.edu>
2019-09-08 17:56:06 -0400

    2.10 done

--- You are awesome/ViewController.swift
+++ You are awesome/ViewController.swift
@@ -20,18 +20,35 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let message1 = "You are Awesome!"
-        let message2 = "You are Great!"
-        let message3 = "You are Awesome!"
         
-        if messagelabel.text == message1 {
-            messagelabel.text = message2
-        } else if messagelabel.text == message2 {
-            messagelabel.text = message3
-        } else{
-            messagelabel.text = message1
-  
+        let messages = ["You are Great!",
+                        "You are Fantastic!!!",
+                        "I love you",
+                        "You are Fantastic",
+                        "When the Genius Bar needs you, they'll call you",
+                        "You brighten my day",
+                        ]
+        var index = 0
+        messagelabel.text = messages[index]
+        
+        if index == messages.count - 1 {
+            index = 0
+        } else {
+            index = index + 1
+        }
+        
+//        let message1 = "You are Awesome!"
+//        let message2 = "You are Great!"
+//        let message3 = "You are Awesome!"
+//
+//        if messagelabel.text == message1 {
+//            messagelabel.text = message2
+//        } else if messagelabel.text == message2 {
+//            messagelabel.text = message3
+//        } else{
+//            messagelabel.text = message1
+//
     }
  
 }
-}
+
