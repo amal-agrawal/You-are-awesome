cb1e69131db3f56320643ad5762e97ed7f727cee
Amal Agrawal <amalagrawal@amals-mbp-2.bc.edu>
2019-09-09 11:49:56 -0400

    2.16 done

--- You are awesome/ViewController.swift
+++ You are awesome/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var awesomeimage: UIImageView!
     @IBOutlet weak var messagelabel: UILabel!
-    var index=0
+    var index = -1
+    var imageindex = -1
+    let numberofimages = 10
     
     //code below executes when the app's view first loads
     override func viewDidLoad() {
@@ -30,36 +33,21 @@ class ViewController: UIViewController {
                         "You brighten my day",
                         ]
         
-        var newindex = -1
+        var newindex : Int
         repeat{
             newindex = Int.random(in:0..<messages.count)
         }
         while index == newindex
         index = newindex
         messagelabel.text = messages[index]
-//        messagelabel.text = messages.randomElement()!
         
+        repeat {
+            newindex = Int.random(in: 0..<numberofimages)
+        } while imageindex == newindex
         
+        imageindex = newindex
+        awesomeimage.image = UIImage(named: "\(imageindex)")
 
-//        messagelabel.text = messages[index]
-//
-//        if index == messages.count - 1 {
-//            index = 0
-//        } else {
-//            index = index + 1
-//        }
-        
-//        let message1 = "You are Awesome!"
-//        let message2 = "You are Great!"
-//        let message3 = "You are Awesome!"
-//
-//        if messagelabel.text == message1 {
-//            messagelabel.text = message2
-//        } else if messagelabel.text == message2 {
-//            messagelabel.text = message3
-//        } else{
-//            messagelabel.text = message1
-//
     }
  
 }
