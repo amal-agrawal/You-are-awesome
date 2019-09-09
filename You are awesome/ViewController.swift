c0ed078206de37d4fc071648d35a71ac331ee8d1
Amal Agrawal <amalagrawal@amals-mbp-2.bc.edu>
2019-09-09 15:06:12 -0400

    2.20 done

--- You are awesome/ViewController.swift
+++ You are awesome/ViewController.swift
@@ -13,13 +13,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var awesomeimage: UIImageView!
     @IBOutlet weak var messagelabel: UILabel!
-    
+    @IBOutlet weak var soundSwitch: UISwitch!
     var awesomeplayer = AVAudioPlayer()
     var index = -1
     var imageindex = -1
     var soundindex = -1
     let numberofimages = 10
-    let numberofsounds = 5
+    let numberofsounds = 10
     
     //code below executes when the app's view first loads
     override func viewDidLoad() {
@@ -37,12 +37,12 @@ class ViewController: UIViewController {
         return newindex
     }
     
-    func playsound(soundName : String) {
+    func playsound(soundName : String, audioPlayer: inout AVAudioPlayer) {
 
         if let sound = NSDataAsset(name: soundName){
             do{
-                try awesomeplayer = AVAudioPlayer(data: sound.data)
-                awesomeplayer.play()
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
             } catch {
                 print("Error: data in \(soundName) couldn't be played as a sound")
             }
@@ -50,6 +50,12 @@ class ViewController: UIViewController {
             print("The sound did not play!")
         }
     }
+    
+    @IBAction func soundSwitchPressed(_ sender: UISwitch) {
+        if soundSwitch.isOn == false && soundindex != -1 {
+            awesomeplayer.stop()
+            }
+        }
     
     @IBAction func showMessagePressed(_ sender: UIButton) {
         
@@ -59,6 +65,9 @@ class ViewController: UIViewController {
                         "You are Fabulous",
                         "When the Genius Bar needs you, they'll call you",
                         "You brighten my day",
+                        "Hope you have a great day!",
+                        "Thank you for being awesome!",
+                        "You are a great person!"
                         ]
     
         
@@ -68,10 +77,12 @@ class ViewController: UIViewController {
         imageindex = nonRepeatingRandom(lastNumber: imageindex, maxValue: numberofimages)
         awesomeimage.image = UIImage(named: "\(imageindex)")
 
-        soundindex = nonRepeatingRandom(lastNumber: soundindex, maxValue: numberofsounds)
-        let soundName = "sound\(soundindex)"
-        playsound(soundName: soundName)
 
+        if soundSwitch.isOn == true {
+            soundindex = nonRepeatingRandom(lastNumber: soundindex, maxValue: numberofsounds)
+            let soundName = "sound\(soundindex)"
+            playsound(soundName: soundName, audioPlayer: &awesomeplayer)
+        }
         
     }
  
